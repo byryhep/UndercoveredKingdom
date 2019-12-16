6f42bd107225eab1c2cb9b865facd1b67d2503c2
byry <byryhep99@gmail.com>
2019-12-16 10:36:41 +0100

    Initial Project

---
+++ Undercovered Kingdom/Assets/Prefab/Camara/CharacterControlRelativo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControlRelativo : MonoBehaviour {
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    //ROTACION
+    public float rot_speed = 2.0f;
+    //Relativo a camara
+    public Transform Ref_Cam;
+
+    //Velocidad en el Aire
+
+    void Update()
+    {
+
+        CharacterController controller = GetComponent<CharacterController>();
+      
+            //CONTROL RELATIVO A CAMARA
+            moveDirection =  (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+
+
+
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+       
+
+
+
+
+        //ROTACION RELATIVO A CAMERA
+        Vector3 temp_Rot_Vector =new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (temp_Rot_Vector.magnitude > 0.1f)
+        {
+
+            transform.forward = temp_Rot_Vector.normalized;
+
+        }
+
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+}
---
+++ Undercovered Kingdom/Assets/Prefab/Camara/followSmoothRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followSmoothRotation : MonoBehaviour
+{
+    public GameObject target;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, target.transform.rotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
